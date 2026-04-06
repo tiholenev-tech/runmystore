0ea56ac61829bdd95d9bc2bacae0cb185bf5e3c3
tiholenev-tech <tvojiat@email.com>
2026-04-06 06:49:09 +0000

    fix config conflict

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
-cat > /var/www/runmystore/config/config.php << 'EOF'
+AIzaSyCiWJltZXfrTbEJXtOlmcuv9tjKYLzVW8
+
 <?php
-define('GEMINI_API_KEY', 'AIzaSyCiWJltZXfrTbEJXtOlmcuv9tjKYLzVW8c');
-define('GEMINI_API_KEY_2', 'AIzaSyCheVI7v344ZW0YdVjzkLYbsCIm-VojqSU');
+define('GEMINI_API_KEY', 'AIzaSyDjTrNXlEfWJ-brqLhjWbkZR0f0yoYScww');
+define('GEMINI_API_KEY_2', 'AIzaSyByii3SeF7QfA3wAW_iqZn5YbSAe7_S5Gc');
 define('GEMINI_MODEL', 'gemini-2.5-flash');
-define('CLAUDE_API_KEY', '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');
-EOF
+define('CLAUDE_API_KEY', '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');
