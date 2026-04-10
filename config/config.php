b8e316a970c672ddba36a463f3dfb7b5313b2635
tiholenev-tech <tvojiat@email.com>
2026-04-10 16:21:58 +0000

    S44 base before S45 changes

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 
 <?php
-define('GEMINI_API_KEY', 'AIzaSyDjTrNXlEfWJ-brqLhjWbkZR0f0yoYScww');
+define('GEMINI_API_KEY', '***REMOVED_GEMINI_KEY_1***');
 define('GEMINI_API_KEY_2', '***REMOVED_GEMINI_KEY_2***');
 define('GEMINI_MODEL', 'gemini-2.5-flash');
 define('CLAUDE_API_KEY', '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');
