1f8517a0c035f696bfdc075620a3de3fea7348d4
tiholenev-tech <tiholenev@gmail.com>
2026-04-12 15:22:07 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,4 +3,4 @@
 define('GEMINI_API_KEY', '***REMOVED_GEMINI_KEY_1***');
 define('GEMINI_API_KEY_2', '***REMOVED_GEMINI_KEY_2***');
 define('GEMINI_MODEL', 'gemini-2.5-flash');
-define('CLAUDE_API_KEY', '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');
+define('CLAUDE_API_KEY', '');
