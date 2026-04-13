6e896f715a508a0fd16be4f155705b63a540f26c
tiholenev-tech <tiholenev@gmail.com>
2026-04-13 11:47:26 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,3 +4,5 @@ define('GEMINI_API_KEY', 'AIzaSyCheVI7v344ZW0YdVjzkLYbsCIm-VojqSU');
 define('GEMINI_API_KEY_2', 'AIzaSyByii3SeF7QfA3wAW_iqZn5YbSAe7_S5Gc');
 define('GEMINI_MODEL', 'gemini-2.5-flash');
 define('CLAUDE_API_KEY', '');
+define('OPENAI_API_KEY', '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');
+define('OPENAI_MODEL', 'gpt-4o-mini');
